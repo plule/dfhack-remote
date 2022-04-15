0ab05be2c823d900dae1733230719b10c1f2ddb6
pierre <pierre>
2022-04-15 21:41:11 +0200

    read elevation example

---
+++ dfhack-remote/examples/read_elevation.rs
@@ -0,0 +1,33 @@
+use bmp::{px, Image, Pixel};
+use dfhack_proto::RemoteFortressReader::WorldMap;
+use dfhack_remote::DFHack;
+
+fn save_world_map(world_map: &WorldMap, dst: String) {}
+
+fn main() {
+    let mut client = DFHack::connect().unwrap();
+
+    let world_map = client.remote_fortress_reader.get_world_map_new().unwrap();
+    let width = world_map.get_world_width() as usize;
+    let height = world_map.get_world_height() as usize;
+
+    let mut img = Image::new(width as u32, height as u32);
+    let tiles = world_map.get_region_tiles();
+    for x in 0..width {
+        for y in 0..height {
+            let tile = &tiles[x + y * width];
+            let elevation = tile.get_elevation();
+            let water_elevation = tile.get_water_elevation();
+            let b = elevation;
+            let above_water = elevation > water_elevation;
+            let g = match above_water {
+                true => elevation,
+                false => 0,
+            };
+            let r = g;
+
+            img.set_pixel(x as u32, y as u32, px!(r, g, b));
+        }
+    }
+    img.save("elevation.bmp").unwrap();
+}
