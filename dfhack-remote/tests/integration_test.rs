afcc167a7831af8fc6278c63ed028a8f18a6bd70
pierre <pierre>
2022-04-16 20:20:44 +0200

    integration test with df process

--- dfhack-remote/tests/integration_test.rs
+++ dfhack-remote/tests/integration_test.rs
@@ -1,5 +1,46 @@
 use dfhack_remote;
 
+#[cfg(feature = "test-with-df")]
+mod withdf {
+    use std::process::Child;
+    use std::sync::Mutex;
+    #[cfg(test)]
+    lazy_static::lazy_static! {
+        static ref DF_PROCESS: Mutex<Option<Child>> = Mutex::new(Option::<Child>::None);
+    }
+
+    #[ctor::ctor]
+    fn init() {
+        use std::{path::PathBuf, process::Command};
+        let df_exe = PathBuf::from(std::env::var("DF_EXE").unwrap());
+        let df_folder = df_exe.parent().unwrap();
+
+        let df = Command::new(&df_exe)
+            .args(["+load-save", "region1"])
+            .current_dir(df_folder)
+            .spawn()
+            .unwrap();
+        let mut process_guard = DF_PROCESS.lock().unwrap();
+        process_guard.replace(df);
+    }
+
+    #[ctor::dtor]
+    fn exit() {
+        let mut process_guard = DF_PROCESS.lock().unwrap();
+        let df = process_guard.take();
+        if let Some(mut df) = df {
+            df.kill().unwrap();
+        }
+    }
+
+    #[test]
+    fn get_version() {
+        let mut client = dfhack_remote::DFHack::connect("127.0.0.1:5000").unwrap();
+        let version = client.core.get_df_version().unwrap();
+        assert!(version.get_value().len() > 0);
+    }
+}
+
 #[test]
 fn sanity_check() {
     let mut message = dfhack_remote::messages::IntMessage::new();
